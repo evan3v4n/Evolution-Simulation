b92bd70186b0fbdf4f2e409ed1ba45ee6f28462e
evan3v4n <evan.scott2050@Gmail.com>
2023-11-06 17:22:55 -0500

    start of project

---
+++ shorelark/libs/neural-network/src/lib.rs
@@ -0,0 +1,28 @@
+
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
