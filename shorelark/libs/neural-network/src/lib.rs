8bde33c0e884dbc063071ca109024ec5da817750
evan3v4n <evan.scott2050@Gmail.com>
2023-12-04 23:43:57 -0500

    updated lib.rs

--- shorelark/libs/neural-network/src/lib.rs
+++ shorelark/libs/neural-network/src/lib.rs
@@ -7,22 +7,33 @@ struct Layer {
     neurons: Vec<Neuron>,
 }
 
+impl Layer {
+    fn propagate(&self,inputs: Vec<f32>) -> Vec<f32> {
+        let mut outputs = Vec::new();
+
+        for neuron in &self.neurons {
+            let output = neuron.propagate(&inputs);
+            outputs.push(output);
+        }
+
+        outputs
+    }
+}
+
 struct Neuron {
     bias: f32,
     weights: Vec<f32>,
 }
 
 impl Network {
-    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        for layer in &self.layers {
+            inputs = layer.propagate(inputs);
+        }
+
+        inputs
     }
 }
 
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+
