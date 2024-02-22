60c2808784846f5cddeb42b3454e67b586f1e36c
evan3v4n <evan.scott2050@Gmail.com>
2024-02-22 04:28:23 -0500

    Neural net
    have to implement tests

--- shorelark/libs/neural-network/src/lib.rs
+++ shorelark/libs/neural-network/src/lib.rs
@@ -1,25 +1,7 @@
+use rand::prelude::*;
 
 pub struct Network {
     layers: Vec<Layer>,
-}
-
-struct Layer {
-    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
-    }
-}
-
-impl Layer {
-    fn propagate(&self,inputs: Vec<f32>) -> Vec<f32> {
-        let mut outputs = Vec::new();
-
-        for neuron in &self.neurons {
-            let output = neuron.propagate(&inputs);
-            outputs.push(output);
-        }
-
-        outputs
-    }
 }
 
 struct Neuron {
@@ -27,13 +9,79 @@ struct Neuron {
     weights: Vec<f32>,
 }
 
+struct Layer {
+    neurons: Vec<Neuron>
+}
+
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
 impl Network {
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        // functional progreamming very Haskall-y
+        // Use rust
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
+
+    pub fn random(layers: &[LayerTopology] ) -> Self {
+        assert!(layers.len() > 1);
+
+        // let mut built_layers = Vec::new();
+
+         let layers = layers
+            .windows(2)
+            .map(|layers| {
+                Layer::random(layers[0].neurons, layers[1].neurons)
+            })
+            .collect();
+
+        Self { layers }
+    }
 }
 
+impl Neuron { 
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        // edge case
+        assert_eq!(inputs.len(), self.weights.len());
 
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
 
+        (self.bias + output).max(0.0)
+    }
+
+    pub fn random(input_size: usize) -> Self {
+        let mut rng = rand::thread_rng();
+
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+    
+        Self { bias, weights }
+    }   
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+         self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+
+    pub fn random(input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(input_size)) 
+            .collect();
+        
+        Self { neurons }
+    }
+}
---
+++ shorelark/libs/neural-network/target/debug/deps/rand_chacha-b2c6cedb6bbcd1cb.d
@@ -0,0 +1,7 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\Evolution-Simulation\shorelark\libs\neural-network\target\debug\deps\rand_chacha-b2c6cedb6bbcd1cb.rmeta: C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\evan1\OneDrive\Documents\GitHub\Evolution-Simulation\shorelark\libs\neural-network\target\debug\deps\rand_chacha-b2c6cedb6bbcd1cb.d: C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\evan1\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs:
